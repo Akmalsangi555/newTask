b0dc94b85cedf62e33b9103b91a757b00295c264
Muhammad Akmal <50768458+Akmalsangi555@users.noreply.github.com>
2023-09-06 18:48:55 +0500

    new
    update

--- lib/view/AddOrder/widgets/customCheckBox.dart
+++ lib/view/AddOrder/widgets/customCheckBox.dart
@@ -20,12 +20,13 @@ class CustomCheckbox extends StatelessWidget {
             width: Get.width* 0.1, // Set the desired width
             height: Get.height* 0.045, // Set the desired height
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(05),
+              borderRadius: BorderRadius.circular(05), 
               border: Border.all(
                 width: 5.0,
                 color: controller.isChecked.value ? AppColor.bgColor : AppColor.textFillColor,
               ),
               color: controller.isChecked.value ? AppColor.textFillColor : Colors.transparent,
+
             ),
             child: controller.isChecked.value
                 ? Icon(Icons.check, size: 30.0, color: AppColor.bgColor) : null,
